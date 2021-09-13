dc3c0a63caeffc7616b8ac25efb79e2c638fa93e
Kyle Wheeler <14957527+KyleWheel@users.noreply.github.com>
2021-09-13 06:57:29 -0700

    Add star svg as component

---
+++ src/ReviewStar.js
@@ -0,0 +1,15 @@
+const React = require("react");
+
+function ReviewStar(props) {
+  return /*#__PURE__*/React.createElement("svg", Object.assign({
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: "none",
+    viewBox: "0 0 10 10"
+  }, props), /*#__PURE__*/React.createElement("polygon", {
+   id:"star",
+   points:"6.91773333 6.1328 10 3.75 6.25 3.75 5 0 3.75 3.75 0 3.75 3.086 6.12293333 1.87493333 10 5.00466667 7.6 8.1276 10"
+  }));
+}
+
+module.exports = ReviewStar;
+
